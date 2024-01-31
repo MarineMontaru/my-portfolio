44eb289849b40bed916a2877a0a79e62b35d613b
MarineMontaru <marine.montaru@oclock.school>
2024-01-31 15:50:46 +0100

    feat/ajout introduction sur page home

--- php/inc/tpl/home.tpl.php
+++ php/inc/tpl/home.tpl.php
@@ -1,1 +1,21 @@
-<p>Bienvenue sur mon site</p>
+<link rel="stylesheet" href="../css/home.css"> 
+
+<h2>Bienvenue sur mon site</h2>
+
+<section class="introduction">
+
+<p>
+    Après 8 années dans le domaine de la Qualité, ma reconversion dans le développement web est apparue comme une évidence. 
+    <br><br>
+    De nature <strong>organisée et rigoureuse</strong>, j'ai toujours aimé bâtir des outils agréables et fonctionnels.
+</p>
+
+<p>
+    Au terme de ma formation Développeur web fullstack PHP/JavaScript, je souhaite mettre mes compétences et mon enthousiasme au service d’une <strong>entreprise dynamique</strong> et aux côtés de collègues <strong>passionnés</strong>.
+</p>
+
+<p>
+    N'hésitez pas à parcourir mon site pour découvrir mon parcours et mes réalisations.
+</p>
+
+</section>
